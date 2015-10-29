e786af04d77a62738cc2387eddf974c932e043be
shangzhixin <shangzhixin@sumaplayer.com>
2015-10-29 14:00:01 +0800

    create demo

--- test4Masonry/ViewController.m
+++ test4Masonry/ViewController.m
@@ -7,21 +7,89 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+{
+    UITextField *_textField;
+}
+
+- (void)dealloc {
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
+    
+    __weak typeof(self) weakSelf = self;
+    
+    _textField = [UITextField new];
+    [_textField setFrame:CGRectMake(20, self.view.frame.size.height-50, 200, 50)];
+    _textField.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:_textField];
+    
+    [_textField mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.left.mas_equalTo(10);
+        make.centerX.equalTo(weakSelf.view);
+        make.bottom.mas_equalTo(10);
+        make.height.mas_equalTo(100);
+    }];
+    
+    // 注册键盘通知
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillChangeFrameNotification:) name:UIKeyboardWillChangeFrameNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHideNotification:) name:UIKeyboardWillHideNotification object:nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)keyboardWillChangeFrameNotification:(NSNotification *)notification {
+    
+    // 获取键盘基本信息（动画时长与键盘高度）
+    NSDictionary *userInfo = [notification userInfo];
+    CGRect rect = [userInfo[UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    CGFloat keyboardHeight = CGRectGetHeight(rect);
+    CGFloat keyboardDuration = [userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    // 修改下边距约束
+    [_textField mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.bottom.mas_equalTo(-keyboardHeight-50);
+    }];
+    
+    // 更新约束
+    [UIView animateWithDuration:keyboardDuration animations:^{
+        
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)keyboardWillHideNotification:(NSNotification *)notification {
+    
+    // 获得键盘动画时长
+    NSDictionary *userInfo = [notification userInfo];
+    CGFloat keyboardDuration = [userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    // 修改为以前的约束（距下边距0）
+    [_textField mas_updateConstraints:^(MASConstraintMaker *make) {
+        
+        make.bottom.mas_equalTo(0);
+    }];
+    
+    // 更新约束
+    [UIView animateWithDuration:keyboardDuration animations:^{
+        
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    [super touchesBegan:touches withEvent:event];
+    [self.view endEditing:YES];
 }
 
 @end
