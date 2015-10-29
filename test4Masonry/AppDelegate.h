fccaecb14a51460774e3e58ec0b09138a3b2f895
shangzhixin <shangzhixin@sumaplayer.com>
2015-10-29 11:44:17 +0800

    Initial Commit

---
+++ test4Masonry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test4Masonry
+//
+//  Created by new on 15-10-29.
+//  Copyright (c) 2015年 new. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test4Masonry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test4Masonry
+//
+//  Created by new on 15-10-29.
+//  Copyright (c) 2015年 new. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
